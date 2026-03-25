1b1900034796aebe2bb4ee19b85982ca955bc12e
Snay-7 <isnaydercandido@gmail.com>
2026-03-25 08:56:21 +0000

    Fix syntax error in image URL assignment

--- src/App.jsx
+++ src/App.jsx
@@ -89,7 +89,7 @@ const DEALS = [
     annualReturn: 24000,
     setupCosts: 3500
   },
-    image: "https://i.ibb.co/YTY86mMm/il-340x270-6800524152-h68o-jpg.webp",,
+    image: "https://i.ibb.co/YTY86mMm/il-340x270-6800524152-h68o-jpg.webp",
 },
 ];
 
